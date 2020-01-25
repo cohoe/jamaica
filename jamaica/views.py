77f8e6c9c614ac82c5ba9043def75c841be304e7
Grant Cohoe <grant@grantcohoe.com>
2020-01-25 17:21:28 -0500

    kinda working

--- jamaica/views.py
+++ jamaica/views.py
@@ -1,10 +1,13 @@
 import json
 import barbados.config
 from jamaica import app, cache
-from barbados.models import CocktailModel
+from barbados.models import CocktailModel, IngredientModel
 from barbados.factories import CocktailFactory
 from barbados.connectors import PostgresqlConnector, RedisConnector
 from flask_api import status, exceptions
+from flask import request
+from barbados.objects.ingredient import IngredientTypeEnum
+from barbados.objects import Ingredient
 
 conn = PostgresqlConnector()
 sess = conn.Session()
@@ -20,6 +23,10 @@ def _list():
     try:
         redis = RedisConnector()
         cocktail_name_list = redis.get(barbados.config.cache.cocktail_name_list_key)
+        pretty = request.args.get('pretty')
+        if pretty:
+            return json.dumps(json.loads(cocktail_name_list), indent=2)
+
         return cocktail_name_list
     except KeyError:
         raise exceptions.APIException('Cache empty or other Redis error.')
@@ -81,3 +88,59 @@ def _get_key_from_cache(cache, key):
             search_results = []
 
     return search_results
+
+
+@app.route('/library/ingredients/')
+def get_ingredients():
+    tree = {}
+    raw_ingredients = sess.query(IngredientModel).all()
+
+    sub_ingredients = []
+
+    for ingredient in raw_ingredients:
+        i = Ingredient(slug=ingredient.slug, display_name=ingredient.display_name, parent=ingredient.parent, type=ingredient.type)
+        if i.type_ is IngredientTypeEnum.CATEGORY:
+            #tree.append(_create_tree_entry(i, partial=False))
+            if i.slug in tree.keys():
+                raise Exception("Duplicate top-level slug %s" % i.slug)
+            tree[i.slug] = _create_tree_entry(i, partial=False)
+        elif i.type_ is IngredientTypeEnum.FAMILY:
+            try:
+                tree[i.parent]['children'][i.slug] = _create_tree_entry(i, partial=False)
+            except KeyError:
+                print("ERROR")
+        elif i.type_ is IngredientTypeEnum.INGREDIENT:
+            try:
+                # There are no top-level ingredients. Always under a family
+                for category_key in tree.keys():
+                    tree[category_key]['children'][i.parent]['children'][i.slug] = _create_tree_entry(i, partial=False)
+            except KeyError:
+                # print("Will parse %s later" % i.slug)
+                sub_ingredients.append(i)
+
+    print(len(sub_ingredients))
+    for i in sub_ingredients:
+        placed = False
+        if i.type_ != IngredientTypeEnum.INGREDIENT:
+            raise Exception("Somehow got a non-ingredient")
+        try:
+            for category_key in tree.keys():
+                for family_key in tree[category_key]['children'].keys():
+                    if i.parent in tree[category_key]['children'][family_key]['children'].keys():
+                        tree[category_key]['children'][family_key]['children'][i.parent]['children'][i.slug] = _create_tree_entry(i, partial=False)
+                        placed = True
+        except KeyError:
+            print("ERROR with %s" % i.slug)
+
+        if not placed:
+            print("Unable to place %s" % i.slug)
+
+    return json.dumps(tree, indent=2)
+
+
+def _create_tree_entry(obj, partial=True):
+    ret = obj.serialize()
+    ret['children'] = {}
+    ret['partial'] = partial
+
+    return ret
