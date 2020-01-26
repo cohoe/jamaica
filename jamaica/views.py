877f7ebe3e4cd4fa364dd369a6a89ece53ce8349
Grant Cohoe <grant@grantcohoe.com>
2020-01-25 23:04:21 -0500

    working tree generation

--- jamaica/views.py
+++ jamaica/views.py
@@ -93,54 +93,56 @@ def _get_key_from_cache(cache, key):
 @app.route('/library/ingredients/')
 def get_ingredients():
     tree = {}
-    raw_ingredients = sess.query(IngredientModel).all()
 
+    # Categories
+    categories = sess.query(IngredientModel).filter(IngredientModel.type == IngredientTypeEnum.CATEGORY.value)
+    for category in categories:
+        c = Ingredient(category.slug, category.display_name, category.type, category.parent)
+        if c.slug in tree.keys():
+            raise Exception("Duplicate top-level slug %s" % c.slug)
+        tree[c.slug] = _create_tree_entry(c)
+
+    # Families
+    family_cache = {} # key=family, value=parent(category)
+    families = sess.query(IngredientModel).filter(IngredientModel.type == IngredientTypeEnum.FAMILY.value)
+    for family in families:
+        f = Ingredient(family.slug, family.display_name, family.type, family.parent)
+        if f.slug in tree[f.parent]['children'].keys():
+            raise Exception("Duplicate family (%s) under %s" % (f.slug, f.parent))
+        tree[f.parent]['children'][f.slug] = _create_tree_entry(f)
+        family_cache[f.slug] = f.parent
+
+    # Ingredients
+    # There are no category-child ingredients. Need two passes to catch everyone
     sub_ingredients = []
+    top_ingredient_cache = {} # key=top_ingredient, value=parent(family)
+    ingredients = sess.query(IngredientModel).filter(IngredientModel.type == IngredientTypeEnum.INGREDIENT.value)
+    for ingredient in ingredients:
+        # i = Ingredient(ingredient.slug, ingredient.display_name, ingredient.type, ingredient.parent)
+        if ingredient.parent in family_cache.keys():
+            i = Ingredient(ingredient.slug, ingredient.display_name, ingredient.type, ingredient.parent)
+            tree[family_cache[i.parent]]['children'][i.parent]['children'][i.slug] = _create_tree_entry(i)
+            top_ingredient_cache[i.slug] = i.parent
+        else:
+            sub_ingredients.append(ingredient)
 
-    for ingredient in raw_ingredients:
-        i = Ingredient(slug=ingredient.slug, display_name=ingredient.display_name, parent=ingredient.parent, type=ingredient.type)
-        if i.type_ is IngredientTypeEnum.CATEGORY:
-            #tree.append(_create_tree_entry(i, partial=False))
-            if i.slug in tree.keys():
-                raise Exception("Duplicate top-level slug %s" % i.slug)
-            tree[i.slug] = _create_tree_entry(i, partial=False)
-        elif i.type_ is IngredientTypeEnum.FAMILY:
-            try:
-                tree[i.parent]['children'][i.slug] = _create_tree_entry(i, partial=False)
-            except KeyError:
-                print("ERROR")
-        elif i.type_ is IngredientTypeEnum.INGREDIENT:
-            try:
-                # There are no top-level ingredients. Always under a family
-                for category_key in tree.keys():
-                    tree[category_key]['children'][i.parent]['children'][i.slug] = _create_tree_entry(i, partial=False)
-            except KeyError:
-                # print("Will parse %s later" % i.slug)
-                sub_ingredients.append(i)
+    for ingredient in sub_ingredients:
+        if ingredient.parent in top_ingredient_cache.keys():
+            i = Ingredient(ingredient.slug, ingredient.display_name, ingredient.type, ingredient.parent)
+            family = top_ingredient_cache[i.parent]
+            category = family_cache[family]
+            tree[category]['children'][family]['children'][i.parent]['children'][i.slug] = _create_tree_entry(i)
+        else:
+            print("Unable to place %s" % ingredient.slug)
 
-    print(len(sub_ingredients))
-    for i in sub_ingredients:
-        placed = False
-        if i.type_ != IngredientTypeEnum.INGREDIENT:
-            raise Exception("Somehow got a non-ingredient")
-        try:
-            for category_key in tree.keys():
-                for family_key in tree[category_key]['children'].keys():
-                    if i.parent in tree[category_key]['children'][family_key]['children'].keys():
-                        tree[category_key]['children'][family_key]['children'][i.parent]['children'][i.slug] = _create_tree_entry(i, partial=False)
-                        placed = True
-        except KeyError:
-            print("ERROR with %s" % i.slug)
-
-        if not placed:
-            print("Unable to place %s" % i.slug)
-
-    return json.dumps(tree, indent=2)
+    return json.dumps(tree)
 
 
-def _create_tree_entry(obj, partial=True):
-    ret = obj.serialize()
-    ret['children'] = {}
-    ret['partial'] = partial
-
-    return ret
+def _create_tree_entry(obj):
+    return ({
+        'slug': obj.slug,
+        'display_name': obj.display_name,
+        # 'type': obj.type_.value,
+        # 'parent': obj.parent,
+        'children': {}
+    })
