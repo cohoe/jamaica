1753ca6ae03bb1cace51c24dce984d520f89f550
Grant Cohoe <grant@grantcohoe.com>
2020-02-07 18:19:06 -0500

    bunch of non-working junk

--- jamaica/api/v1/ingredients/controllers.py
+++ jamaica/api/v1/ingredients/controllers.py
@@ -23,6 +23,56 @@ def _list():
 
 
 @app.route('/ingredients/tree')
+def build_tree():
+    parent_cache = {}
+    tree, parent_cache = _add_to_tree(IngredientTypes.CATEGORY)
+    tree, parent_cache = _add_to_tree(IngredientTypes.FAMILY, tree=tree, parent_cache=parent_cache)
+
+    return tree
+
+
+class TreeObject:
+    def __init__(self, i, parent_cache):
+        self.i = i
+        self.parent_cache = parent_cache
+
+    @property
+    def tree_entry(self):
+        path = {}
+
+        entry = _create_tree_entry(self.i)
+
+        if self.i.type_ == IngredientTypes.CATEGORY:
+            path = {self.i.slug: entry}
+        elif self.i.type_ == IngredientTypes.FAMILY:
+            path = {self.i.parent: {'children': {self.i.slug: entry}}}
+
+        return path
+
+
+def _add_to_tree(type_, tree=None, parent_cache=None):
+    if not tree:
+        tree = {}
+    if not parent_cache:
+        parent_cache = {}
+
+    items = IngredientModel.get_by_type(type_)
+    for item in items:
+        i = Ingredient(item.slug, item.display_name, item.type, item.parent)
+
+        to = TreeObject(i, parent_cache)
+
+        # if i.slug in search_scope:
+        #     raise Exception("Duplicate child (%s) under %s" % (i.slug, i.parent))
+        #
+        # search_scope[i.slug] = _create_tree_entry(i)
+        tree.update(to.tree_entry)
+        parent_cache[i.slug] = i.parent
+
+    return tree, parent_cache
+
+
+@app.route('/ingredients/treeold')
 def get_ingredients():
     tree = {}
 
@@ -35,7 +85,7 @@ def get_ingredients():
         tree[c.slug] = _create_tree_entry(c)
 
     # Families
-    family_cache = {} # key=family, value=parent(category)
+    family_cache = {}  # key=family, value=parent(category)
     families = IngredientModel.get_by_type(IngredientTypes.FAMILY)
     for family in families:
         f = Ingredient(family.slug, family.display_name, family.type, family.parent)
@@ -47,7 +97,7 @@ def get_ingredients():
     # Ingredients
     # There are no category-child ingredients. Need two passes to catch everyone
     sub_ingredients = []
-    top_ingredient_cache = {} # key=top_ingredient, value=parent(family)
+    top_ingredient_cache = {}  # key=top_ingredient, value=parent(family)
     ingredients = IngredientModel.get_by_type(IngredientTypes.INGREDIENT)
     for ingredient in ingredients:
         # i = Ingredient(ingredient.slug, ingredient.display_name, ingredient.type, ingredient.parent)
