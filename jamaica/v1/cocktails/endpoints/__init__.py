091a5e0ce8ab9b19259cbeced8ef21c0f0a0caea
Grant Cohoe <grant@grantcohoe.com>
2020-04-11 18:03:01 -0400

    add all cocktail endpoints

--- jamaica/v1/cocktails/endpoints/__init__.py
+++ jamaica/v1/cocktails/endpoints/__init__.py
@@ -8,6 +8,9 @@ from jamaica.v1.cocktails.parsers import cocktail_list_parser
 
 from barbados.search.cocktail import CocktailSearch
 from barbados.caches import CocktailNameCache
+from barbados.models import CocktailModel
+from barbados.factories import CocktailFactory
+from barbados.serializers import ObjectSerializer
 
 ns = api.namespace('v1/cocktails', description='Cocktail recipes.')
 
@@ -19,6 +22,19 @@ class CocktailsCollection(Resource):
     @api.marshal_list_with(cocktail_list_result)
     def get(self):
         return CocktailSearch(**request.args).execute()
+
+
+@ns.route('/<string:slug>')
+@api.response(404, 'Cocktail slug not in database.')
+class CocktailCollection(Resource):
+
+    def get(self, slug):
+        try:
+            result = CocktailModel.get_by_slug(slug)
+            c = CocktailFactory.model_to_obj(result)
+            return ObjectSerializer.serialize(c, 'dict')
+        except KeyError:
+            ns.abort(404, 'Cocktail not found.', slug=slug)
 
 
 @ns.route('/index')
