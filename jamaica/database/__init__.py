d8561e451815ad3dbdef7c3a742e60252525160f
Grant Cohoe <grant@grantcohoe.com>
2020-04-16 21:08:48 -0400

    cleanup england, rebrand AppConfig to registry, make jamaica pull db creds from registry

--- jamaica/database/__init__.py
+++ jamaica/database/__init__.py
@@ -1,3 +1,8 @@
 from barbados.connectors import PostgresqlConnector
+from barbados.services import Registry
 
-pgconn = PostgresqlConnector(database='amari', username='postgres', password='s3krAt')
+db_database = Registry.get('/database/postgres/database')
+db_username = Registry.get('/database/postgres/username')
+db_password = Registry.get('/database/postgres/password')
+
+pgconn = PostgresqlConnector(database=db_database, username=db_username, password=db_password)
