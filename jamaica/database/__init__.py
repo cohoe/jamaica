3f76e6d7df1ccdb7157318dd7a072e451fcfd923
Grant Cohoe <grant@grantcohoe.com>
2020-04-16 22:19:45 -0400

    docs and restructure

--- jamaica/database/__init__.py
+++ jamaica/database/__init__.py
@@ -1,8 +1,3 @@
-from barbados.connectors import PostgresqlConnector
 from barbados.services import Registry
 
-db_database = Registry.get('/database/postgres/database')
-db_username = Registry.get('/database/postgres/username')
-db_password = Registry.get('/database/postgres/password')
-
-pgconn = PostgresqlConnector(database=db_database, username=db_username, password=db_password)
+pgconn = Registry.get_database_connection()
