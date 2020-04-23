e8481483a54f2d2aef79b1add3cae624d3e6e7e7
Grant Cohoe <grant@grantcohoe.com>
2020-04-23 01:04:24 -0400

    add inverted components

--- jamaica/v1/cocktails/parsers.py
+++ jamaica/v1/cocktails/parsers.py
@@ -3,6 +3,7 @@ from flask_restx import reqparse
 cocktail_list_parser = reqparse.RequestParser()
 cocktail_list_parser.add_argument('name', type=str, help='Partial cocktail or spec name.')
 cocktail_list_parser.add_argument('components', type=str, action='split', help='Comma-seperated list of components.')
+cocktail_list_parser.add_argument('no_components', type=str, action='split', help='Comma-seperated list of components to exclude.')
 cocktail_list_parser.add_argument('alpha', type=str, help='Single character to search by (or # aka %23 for numbers).')
 cocktail_list_parser.add_argument('construction', type=str, help='Drink construction.')
 cocktail_list_parser.add_argument('component_count', type=int, help='Count of components.')
