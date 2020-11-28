68b0ea1cb942fc11c2553e6c94fa46106a559d7e
Grant Cohoe <grant@grantcohoe.com>
2020-11-27 19:06:43 -0500

    add cache get

--- jamaica/v1/caches/endpoints/__init__.py
+++ jamaica/v1/caches/endpoints/__init__.py
@@ -1,9 +1,23 @@
+import json
 from flask_restx import Resource
 from jamaica.v1.restx import api
 
 from barbados.caches import cache_factory
 
 ns = api.namespace('v1/caches', description='Caches.')
+
+
+@ns.route('/')
+class CachesEndpoint(Resource):
+
+    @api.response(200, 'success')
+    def get(self):
+        """
+        Return a list of all cache keys.
+        :return: List[Dict]
+        """
+        keys = cache_factory.cache_keys()
+        return list(keys)
 
 
 @ns.route('/<string:key>')
