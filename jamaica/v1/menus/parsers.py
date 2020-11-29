7c7add64694343ddf8a0f21b18af145795bd4f36
Grant Cohoe <grant@grantcohoe.com>
2020-11-28 19:02:20 -0500

    search

--- jamaica/v1/menus/parsers.py
+++ jamaica/v1/menus/parsers.py
@@ -2,3 +2,4 @@ from flask_restx import reqparse
 
 menu_list_parser = reqparse.RequestParser()
 menu_list_parser.add_argument('cocktail_slug', type=str, help='Partial cocktail slug.')
+menu_list_parser.add_argument('name', type=str, help='Name of the menu.')
