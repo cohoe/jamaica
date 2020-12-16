1322f5a34925a3ee0d16d29c88d02428a3290192
Grant Cohoe <grant@grantcohoe.com>
2020-12-15 22:01:26 -0500

    todo and fixes

--- jamaica/v1/inventories/endpoints/__init__.py
+++ jamaica/v1/inventories/endpoints/__init__.py
@@ -152,7 +152,9 @@ class InventoryRecipesEndpoint(Resource):
 class InventoryRecipesEndpoint(Resource):
 
     def get(self, id):
-        # @TODO id
+        # @TODO ponder a better way to do this. It can't go in the parser itself because
+        # that lets end users specify their own value.
+        inventory_recipes_parser.add_argument('inventory_id', default=str(id))
         args = inventory_recipes_parser.parse_args(strict=True)
 
         # Don't return any results if all parameters are empty.
