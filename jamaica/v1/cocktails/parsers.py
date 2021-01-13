81b3b128010e52d71e8330489f6c58e5d7443747
Grant Cohoe <grant@grantcohoe.com>
2021-01-12 21:29:03 -0500

    add garnish boolean search

--- jamaica/v1/cocktails/parsers.py
+++ jamaica/v1/cocktails/parsers.py
@@ -1,4 +1,4 @@
-from flask_restx import reqparse
+from flask_restx import reqparse, inputs
 
 cocktail_list_parser = reqparse.RequestParser()
 cocktail_list_parser.add_argument('name', type=str, help='Partial cocktail or spec name.')
@@ -7,3 +7,5 @@ cocktail_list_parser.add_argument('no_components', type=str, action='split', hel
 cocktail_list_parser.add_argument('alpha', type=str, help='Single character to search by (or # aka %23 for numbers).')
 cocktail_list_parser.add_argument('construction', type=str, help='Drink construction.')
 cocktail_list_parser.add_argument('component_count', type=int, help='Count of components.')
+# https://github.com/noirbizarre/flask-restplus/issues/199
+cocktail_list_parser.add_argument('garnish', type=inputs.boolean, help='Boolean of if results should have garnish.')
