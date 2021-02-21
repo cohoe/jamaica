636d14261d7bab9227ed4d8f6e688081282044a5
Grant Cohoe <grant@grantcohoe.com>
2021-02-20 22:57:07 -0500

    cleanup todos

--- jamaica/v1/inventories/parsers.py
+++ jamaica/v1/inventories/parsers.py
@@ -5,7 +5,6 @@ from jamaica.v1.cocktails.parsers import cocktail_list_parser
 inventory_resolve_parser = reqparse.RequestParser()
 inventory_resolve_parser.add_argument('cocktail', type=str, help='cocktail slug')
 inventory_resolve_parser.add_argument('spec', type=str, help='spec slug from the cocktail')
-# @TODO inventory resolution
 # inventory_resolve_parser.add_argument('ingredient', type=str, help='cocktail slug')
 
 # This matches everything that the cocktail_list_parser can do.
