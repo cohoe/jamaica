fbae22c31fae8ed38805fe3b94a254f5e358ec84
Grant Cohoe <grant@grantcohoe.com>
2021-07-16 00:23:11 -0400

    fix searching based on component count

--- jamaica/v1/cocktails/parsers.py
+++ jamaica/v1/cocktails/parsers.py
@@ -6,7 +6,7 @@ cocktail_list_parser.add_argument('components', type=str, action='split', help='
 cocktail_list_parser.add_argument('no_components', type=str, action='split', help='Comma-seperated list of components to exclude.')
 cocktail_list_parser.add_argument('alpha', type=str, help='Single character to search by (or # aka %23 for numbers).')
 cocktail_list_parser.add_argument('construction', type=str, help='Drink construction.')
-cocktail_list_parser.add_argument('component_count', type=int, help='Count of components.')
+cocktail_list_parser.add_argument('component_count', type=str, help='Count of components. This is a string because it can be a range.')
 # https://github.com/noirbizarre/flask-restplus/issues/199
 cocktail_list_parser.add_argument('garnish', type=inputs.boolean, help='Boolean of if results should have garnish.')
 cocktail_list_parser.add_argument('citation_name', type=str, help='Citation name keyword')
